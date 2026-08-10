55f8cc66c11e81aa3356bfa25ac4cee98c9bbbac
Sanskar-12 <sanskarv2004@gmail.com>
2026-08-10 16:54:22 +0530

    Feature: Made billing controllers like create order and verify payment route

--- services/billing/routes/billing.route.js
+++ services/billing/routes/billing.route.js
@@ -1,5 +1,8 @@
 import express from "express";
-import { createOrder, verifyPayment } from "../controllers/billing.controller";
+import {
+  createOrder,
+  verifyPayment,
+} from "../controllers/billing.controller.js";
 
 const router = express.Router();
 
