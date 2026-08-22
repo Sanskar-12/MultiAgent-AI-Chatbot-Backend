26abfcd4c8d164191b2b53238b0fb0ee2013acbb
Sanskar-12 <sanskarv2004@gmail.com>
2026-08-22 15:25:58 +0530

    Feature: Made Pdf -RAG Agent

--- services/agent/agents/pdfRag.agent.js
+++ services/agent/agents/pdfRag.agent.js
@@ -1,1 +1,70 @@
-export const pdfRag = (state) => {};
+import fs from "fs";
+import { PDFParse } from "pdf-parse";
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+import vectorStore from "../config/vectorDb.js";
+import { getModel } from "../config/models.js";
+import { HumanMessage, SystemMessage } from "@langchain/core/messages";
+
+export const pdfRag = async (state) => {
+  try {
+    const buffer = fs.readFileSync(state.file.path);
+
+    // convert buffer into pdf
+    const pdf = new PDFParse({
+      data: buffer,
+    });
+
+    const result = await pdf.getText();
+    const text = result.text;
+
+    const splitter = new RecursiveCharacterTextSplitter({
+      chunkSize: 1000,
+      chunkOverlap: 200,
+    });
+
+    const docs = await splitter.createDocuments([text]);
+
+    const collectionName = `pdf-${Date.now()}`;
+
+    const store = await vectorStore(docs, collectionName);
+
+    const relevantDocs = await store.similaritySearch(state.prompt, 5);
+
+    const context = relevantDocs.map((d) => d.pageContent).join("\n\n");
+
+    const llm = await getModel("pdfrag");
+
+    const messages = [
+      new SystemMessage(
+        `
+    You are CortexAI PDF Assistant.
+
+    Rules:
+
+    - Answer ONLY from the uploaded PDF.
+    - Never make up information.
+    - If the answer is not present in the PDF, reply: "I couldn't find the information in the uploaded PDF."
+    - Use Markdown formatting.
+    `,
+      ),
+      new HumanMessage(
+        `
+        Context: ${context}
+        Question: ${state.prompt}
+        `,
+      ),
+    ];
+
+    const response = llm.invoke(messages);
+
+    return {
+      ...state,
+      aiResponse: response.content,
+    };
+  } catch (error) {
+    console.log(error);
+    return { ...state, aiResponse: "Something went wrong processing the PDF." };
+  } finally {
+    fs.unlinkSync(state.file.path);
+  }
+};
